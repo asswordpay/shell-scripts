62936462ddb340978eb0a12284e27bedfde961ff
asswordpay <75650924+asswordpay@users.noreply.github.com>
2021-08-15 16:08:04 -0400

    Create ringworm.sh

---
+++ ringworm.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+clear
+echo " L       k                                   d                     h    b            "
+echo " L    ii k k                                 d              ii     h    b            "
+echo " L       kk   eee      aa     ggg ooo ooo  ddd     nnn  eee    ggg hhh  bbb  ooo rrr "
+echo " L    ii k k  e e     a a     g g o o o o d  d     n  n e e ii g g h  h b  b o o r   "
+echo " LLLL ii k  k ee      aaa     ggg ooo ooo  ddd     n  n ee  ii ggg h  h bbb  ooo r   "
+echo "                               g                                g                   "
+echo "                             ggg                              ggg              "
+sleep 3
+clear
+echo "RRRR                                      EEEE      l                "
+echo "R   R ii                                  E         l     ii         "
+echo "RRRR     nnn  ggg w   w ooo rrr mmmm      EEE   ccc l  aa    rrr eee "
+echo "R R   ii n  n g g w w w o o r   m m m     E    c    l a a ii r   e e "
+echo "R  RR ii n  n ggg  w w  ooo r   m m m     EEEE  ccc l aaa ii r   ee  "
+echo "                g                                                    "
+echo "              ggg                                                    "
+rm ringworm.sh
